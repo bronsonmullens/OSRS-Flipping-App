ab0a2a502b14c8df249d04ff11e2855a87c5d68b
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-26 01:06:43 -0800

    Set up a temporary UI for testing network calls.

---
+++ OSRS Flipping App/View Controllers/DashboardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardViewController.swift
+//  OSRS Flipping App
+//
+//  Created by Bronson Mullens on 2/26/21.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    
+    @IBOutlet weak var searchButtonTapped: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
