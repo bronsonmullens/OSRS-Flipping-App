e86875b4eac54c5789a72e8985a483ef88cbb4fc
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-26 00:58:14 -0800

    Created Xcode project and organized files. Set up .gitignore as well.

---
+++ OSRS Flipping App/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OSRS Flipping App
+//
+//  Created by Bronson Mullens on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
